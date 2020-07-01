1eb66bc5711e71551e067c527cc665f2d56277a1
cmt2 <cmt2@berkeley.edu>
2020-07-01 12:11:45 -0700

    adding print statements to bayou_Rscript.R

--- bayou/bayou_Rscript.R
+++ bayou/bayou_Rscript.R
@@ -1,9 +1,11 @@
 # execute bayou fun with scriptR
 
 # direct to library for packages
+print("setting new lib path as: /global/home/users/cmt2/R/x86_64-pc-linux-gnu-library/3.6")
 .libPaths(new = "/global/home/users/cmt2/R/x86_64-pc-linux-gnu-library/3.6")
 # get the arguments
 args <- commandArgs(trailingOnly = TRUE)
+print(paste0("args: ", args))
 #args = c("--i", 1, "--clim_var", "bio4", "--run_num", 2, "--ngen", "5000")
 
 
@@ -14,7 +16,9 @@ args <- commandArgs(trailingOnly = TRUE)
 
 # load libraries and source function
 source("bayou/run_bayou.R")
+print("sourced run_bayou.R")
 library(bayou)
+print("libraries loaded")
 
 run_bayou(i = as.integer(.i), 
           clim_var = .clim_var, 
