a7d27c94dbec3db2685c36ce2eb44bcc31cfeabf
cmt2 <cmt2@berkeley.edu>
2020-07-01 14:43:38 -0700

    adding echos to .sh

--- bayou/bayou_all.sh
+++ bayou/bayou_all.sh
@@ -4,37 +4,34 @@
 #SBATCH --qos=savio_normal
 #SBATCH --mail-user=cmt2@berkeley.edu
 #SBATCH --mail-type=ALL
-#SBATCH --time=72:00:00
+#SBATCH --time=00:30:00
 
 cd /global/scratch/cmt2/underground_evo
-
+echo loading modules
 module load r/3.6.3
 module load r-packages/default
 module load r-spatial/2018-12-05-r35
 module load imagemagick/7.0.8-29
-
-Rscript bayou/run_bayou.R --i 1 --clim_var "bio15" --run_num 1 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 1 --clim_var "bio15" --run_num 2 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 1 --clim_var "bio4" --run_num 1 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 1 --clim_var "bio4" --run_num 2 --ngen 10000000 &
- 
-Rscript bayou/run_bayou.R --i 2 --clim_var "bio15" --run_num 1 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 2 --clim_var "bio15" --run_num 2 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 2 --clim_var "bio4" --run_num 1 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 2 --clim_var "bio4" --run_num 2 --ngen 10000000 &
- 
-Rscript bayou/run_bayou.R --i 3 --clim_var "bio15" --run_num 1 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 3 --clim_var "bio15" --run_num 2 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 3 --clim_var "bio4" --run_num 1 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 3 --clim_var "bio4" --run_num 2 --ngen 10000000 &
- 
-Rscript bayou/run_bayou.R --i 4 --clim_var "bio15" --run_num 1 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 4 --clim_var "bio15" --run_num 2 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 4 --clim_var "bio4" --run_num 1 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 4 --clim_var "bio4" --run_num 2 --ngen 10000000 &
- 
-Rscript bayou/run_bayou.R --i 5 --clim_var "bio15" --run_num 1 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 5 --clim_var "bio15" --run_num 2 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 5 --clim_var "bio4" --run_num 1 --ngen 10000000 &
-Rscript bayou/run_bayou.R --i 5 --clim_var "bio4" --run_num 2 --ngen 10000000;
+echo finished loading modules
+echo starting r runs
+Rscript bayou/run_bayou.R --i 1 --clim_var bio15 --run_num 1 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 1 --clim_var bio15 --run_num 2 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 1 --clim_var bio4 --run_num 1 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 1 --clim_var bio4 --run_num 2 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 2 --clim_var bio15 --run_num 1 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 2 --clim_var bio15 --run_num 2 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 2 --clim_var bio4 --run_num 1 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 2 --clim_var bio4 --run_num 2 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 3 --clim_var bio15 --run_num 1 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 3 --clim_var bio15 --run_num 2 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 3 --clim_var bio4 --run_num 1 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 3 --clim_var bio4 --run_num 2 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 4 --clim_var bio15 --run_num 1 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 4 --clim_var bio15 --run_num 2 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 4 --clim_var bio4 --run_num 1 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 4 --clim_var bio4 --run_num 2 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 5 --clim_var bio15 --run_num 1 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 5 --clim_var bio15 --run_num 2 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 5 --clim_var bio4 --run_num 1 --ngen 10000000 &
+Rscript bayou/run_bayou.R --i 5 --clim_var bio4 --run_num 2 --ngen 10000000;
 wait; 
