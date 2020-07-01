f4ea783b0a7b2a91da76ab322e31993888576c6b
cmt2 <cmt2@berkeley.edu>
2020-07-01 12:29:46 -0700

    updating modules for .sh file

--- bayou/bayou_all.sh
+++ bayou/bayou_all.sh
@@ -9,29 +9,32 @@
 cd /global/scratch/cmt2/underground_evo
 
 module load r/3.6.3
+module load r-packages/default
+module load r-packages/r-spatial/2018-12-05-r35
+module load imagemagick/7.0.8-29
 
 Rscript bayou/run_bayou.R --i 1 --clim_var "bio15" --run_num 1 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 1 --clim_var "bio15" --run_num 2 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 1 --clim_var "bio4" --run_num 1 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 1 --clim_var "bio4" --run_num 2 --ngen 10000000 &
-
+ 
 Rscript bayou/run_bayou.R --i 2 --clim_var "bio15" --run_num 1 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 2 --clim_var "bio15" --run_num 2 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 2 --clim_var "bio4" --run_num 1 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 2 --clim_var "bio4" --run_num 2 --ngen 10000000 &
-
+ 
 Rscript bayou/run_bayou.R --i 3 --clim_var "bio15" --run_num 1 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 3 --clim_var "bio15" --run_num 2 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 3 --clim_var "bio4" --run_num 1 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 3 --clim_var "bio4" --run_num 2 --ngen 10000000 &
-
+ 
 Rscript bayou/run_bayou.R --i 4 --clim_var "bio15" --run_num 1 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 4 --clim_var "bio15" --run_num 2 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 4 --clim_var "bio4" --run_num 1 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 4 --clim_var "bio4" --run_num 2 --ngen 10000000 &
-
+ 
 Rscript bayou/run_bayou.R --i 5 --clim_var "bio15" --run_num 1 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 5 --clim_var "bio15" --run_num 2 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 5 --clim_var "bio4" --run_num 1 --ngen 10000000 &
 Rscript bayou/run_bayou.R --i 5 --clim_var "bio4" --run_num 2 --ngen 10000000;
-wait;
+wait; 
