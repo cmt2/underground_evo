98f443f405c283ed7997d595657197c3480e18a2
cmt2 <cmt2@berkeley.edu>
2021-11-09 11:45:26 -0800

    adding bayou code for tree 11

---
+++ bayou/scripts/bayou_run_tree11.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --partition=savio
+#SBATCH --account=fc_rothfelslab
+#SBATCH --qos=savio_normal
+#SBATCH --mail-user=cmt2@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --time=72:00:00
+
+cd /global/scratch/cmt2/underground_evo
+echo loading modules
+module load r/3.6.3
+module load r-packages/default
+module load r-spatial/2020-11-30-r36
+module load imagemagick/7.0.8-29
+echo finished loading modules
+echo starting r runs
+Rscript bayou/scripts/bayou_Rscript.R --i 11  --clim_var bio15 --run_num 1 --ngen 3000000 &
+Rscript bayou/scripts/bayou_Rscript.R --i 11  --clim_var bio4  --run_num 1 --ngen 3000000 &
+Rscript bayou/scripts/bayou_Rscript.R --i 11  --clim_var bio15 --run_num 2 --ngen 3000000 &
+Rscript bayou/scripts/bayou_Rscript.R --i 11  --clim_var bio4  --run_num 2 --ngen 3000000 &
+Rscript bayou/scripts/bayou_Rscript.R --i 11  --clim_var bio15 --run_num 3 --ngen 3000000 &
+Rscript bayou/scripts/bayou_Rscript.R --i 11  --clim_var bio4  --run_num 3 --ngen 3000000 &
+Rscript bayou/scripts/bayou_Rscript.R --i 11  --clim_var bio15 --run_num 4 --ngen 3000000 &
+Rscript bayou/scripts/bayou_Rscript.R --i 11  --clim_var bio4  --run_num 4 --ngen 3000000 &
+wait; 
